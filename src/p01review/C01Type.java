fa15d0b7d3c984c75f76915300d1cafbf7a8ab10
ka-jiji <minttaeook1@naver.com>
2021-09-01 08:37:17 +0700

    data type

---
+++ src/p01review/C01Type.java
@@ -0,0 +1,27 @@
+package p01review;
+
+public class C01Type {
+
+	public static void main(String[] args) {
+		//데이터 타입
+		//1. 기본타입 (primitive type)
+//		8개
+//		byte(1) , short(2) , int (4), long(8)
+//		float(4) , double(8)
+//		char(2)
+//		booblean(1)
+		
+		int i = 4;
+		int j = i;
+		
+		
+		//2. 참조타입 (reference type)
+		
+		int[] arr = {3,4};
+		int[] brr = arr;		
+		brr[0] = 33;
+		
+		
+		
+	}
+}
